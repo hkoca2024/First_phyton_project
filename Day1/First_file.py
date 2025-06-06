27e6ec7f95d94b299ffa638648901adafab64f9c
90542 <hamit.koca@gmail.com>
2025-06-06 15:09:40 +0300

    first phyton programming is added.

---
+++ Day1/First_file.py
@@ -0,0 +1,12 @@
+
+print("Hello world")
+s=("35")
+print(type(s))
+n=int(s)
+
+print(n)
+age=53
+print(f"I am {age} years old")
+prog_language="python"
+version=3.13
+print(f"my programming language is {prog_language} and version {version}.")
